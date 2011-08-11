80197274f6796badaf0d5be7906fc1c83978a653
Pierre Jacob <pierre.jacob@ensae.fr>
2011-08-11 00:00:00 +0000

    version 0.3

---
+++ R/preexploration.R
@@ -0,0 +1,17 @@
+preexplorationAMH <- function(target, nchains, niterations, proposal){
+    cat("Pre exploration to get log energy range\n")
+    preexpparameters <- tuningparameters(nchains = nchains, niterations = niterations,
+                                         adaptiveproposal = TRUE, storeall = FALSE)
+    if (missing(proposal))
+      preexp <- adaptiveMH(target, preexpparameters)
+    else 
+      preexp <- adaptiveMH(target, preexpparameters, proposal)
+    burnin <- min(1000, niterations / 10)
+    logtarget <- as.vector(preexp$alllogtarget[burnin:(niterations + 1),])
+    LogEnergyRange <- range(-logtarget)
+    LogEnergyQtile <- as.numeric(quantile(-logtarget, probs = 0.1))
+    return(list(LogEnergyRange = LogEnergyRange, 
+                LogEnergyQtile = LogEnergyQtile,
+                SuggestedRange = c(LogEnergyQtile, LogEnergyRange[2]),
+                finalchains = preexp$finalchains))
+}
---
+++ R/targetclass.R
@@ -0,0 +1,57 @@
+setClass("target",
+         representation(dimension="numeric", parameters="list",
+                        type = "character", name = "character",
+                        rinit = "function", generate = "function",
+                        logdensity = "function", logdensityupdate =
+                        "function", updateavailable = "logical"))
+setGeneric("target", function(...)standardGeneric("target"))
+target.constructor <- function(..., dimension, parameters, name,
+                               type, rinit, logdensity, generate,
+                               logdensityupdate){
+  if (missing(dimension))
+    stop(sQuote("dimension"), "has to be specified.")
+  if (missing(parameters))
+    parameters <- list()
+  if (missing(name))
+    name <- "unspecified"
+  if (missing(type))
+    type <- "continuous"
+  if (missing(rinit))
+    stop(sQuote("rinit"), "has to be specified to draw initial points of the Markov chains")
+  if (missing(logdensity))
+    stop(sQuote("logdensity"), "has to be specified (up to an (additive) normalizing constant)")    
+  if (missing(generate))
+      generate <- function(size, parameters, ...) stop(sQuote("generate"), "is not specified")
+  if (missing(logdensityupdate)){
+      logdensityupdate <- function(...) NULL
+      updateavailable <- FALSE
+  } else { updateavailable <- TRUE }
+
+  new("target", dimension = dimension, parameters = parameters,
+      name = name, type = type, rinit = rinit, logdensity = logdensity,
+      generate = generate, logdensityupdate = logdensityupdate,
+      updateavailable = updateavailable)
+}
+setMethod("target",
+          definition = function(..., dimension, parameters, name, type,
+                                rinit, logdensity, generate, logdensityupdate){
+            target.constructor(dimension = dimension,
+                               parameters = parameters, name = name,
+                               type = type, rinit = rinit,
+                               logdensity = logdensity,
+                               generate = generate, logdensityupdate =
+                               logdensityupdate)
+          })
+
+setMethod(f = "show", signature = "target",
+          def = function(object){
+            cat("Object of class ", class(object), ".\n", sep = "")
+            cat("*name:", object@name, "\n")
+            cat("*type:", object@type, "\n")
+            cat("*dimension of the state space:", object@dimension, "\n")
+            cat("*target parameters:", names(object@parameters), "\n")
+            cat("*update available:", object@updateavailable, "\n")
+          })
+
+
+
